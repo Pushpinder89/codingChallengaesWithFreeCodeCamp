1aa4d7c1fbe0215acd36b220f2cf0636abd2baea
PKaur <dgninder@gmail.com>
2019-03-01 10:54:09 -0800

    reverse an Array

---
+++ ReverseAnArray.js
@@ -0,0 +1,12 @@
+// reverse an array
+
+var a = [1,4,3,2];
+
+// Complete the reverseArray function below.
+function reverseArray(a) {
+    var newArray = [];
+        for (var i = a.length-1; i >= 0; i--)
+            newArray.push(a[i]);
+        return newArray;
+    // I could do a.reverse(); but wanted to solve it with this function's help.
+    }
